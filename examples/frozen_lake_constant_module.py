7f8a054dda0e572190493f85ec85081a7b0885e8
mjlaali <mjlaali@gmail.com>
2024-03-13 21:16:25 -0700

    Solve FrozenLake with constant module

--- examples/frozen_lake_constant_module.py
+++ examples/frozen_lake_constant_module.py
@@ -9,7 +9,6 @@ from torchrl.envs import (
     EnvBase,
     step_mdp,
 )
-from torchrl.modules import QValueActor
 
 from mcts.mcts_policy import (
     SimulatedSearchPolicy,
@@ -17,17 +16,9 @@ from mcts.mcts_policy import (
     UpdateTreeStrategy,
     AlphaZeroExpansionStrategy,
     PuctSelectionPolicy,
-    ConstantValueExpansion,
-    UcbSelectionPolicy,
 )
 from mcts.stateless_frozenlake import StatelessFrozenLake
 from mcts.tensordict_map import TensorDictMap
-
-
-def make_q_value(num_observation, num_action, action_space):
-    net = torch.nn.Linear(num_observation, num_action)
-    qvalue_module = QValueActor(net, in_keys=["observation"], action_space=action_space)
-    return qvalue_module
 
 
 def make_env() -> EnvBase:
@@ -45,15 +36,6 @@ def make_env() -> EnvBase:
 def main():
     torch.manual_seed(1)
     env = make_env()
-    # qvalue_module = make_q_value(
-    #     env.observation_spec["observation"].shape[-1],
-    #     env.action_spec.shape[-1],
-    #     env.action_spec,
-    # )
-    # print(qvalue_module(env.reset()))
-
-    # loss_module = DQNLoss(qvalue_module, action_space=env.action_spec)
-
     tree = TensorDictMap(["observation", "step_count"])
 
     mcts_policy = MctsPolicy(
@@ -72,17 +54,10 @@ def main():
         tree_updater=UpdateTreeStrategy(
             tree,
         ),
-        env=make_env(),
-        num_simulation=10000,
-        max_steps=10,
+        env=env,
+        num_simulation=2000,
+        max_steps=100,
     )
-
-    # res = policy(env.reset())
-
-    # for k in ("action_value", "action_count", "action_value", "action"):
-    #     print(f"{k}:\n{res[k].detach().numpy()}")
-    #
-    # print("start exploring")
 
     def render(an_env: EnvBase, next_state: TensorDictBase) -> None:
         print("\n------------")
@@ -99,16 +74,10 @@ def main():
 
         print(an_env.render())
 
-    rollout = env.rollout(policy=policy, max_steps=4, callback=render)
-
+    env.reset()
+    rollout = env.rollout(policy=policy, max_steps=20, callback=render)
     last_state = step_mdp(rollout[-1])
     render(env, last_state)
-
-    # print("rollout actions")
-    # print(rollout["action"])
-    #
-    # print("reward")
-    # print(rollout[("next", "reward")])
 
 
 if __name__ == "__main__":
